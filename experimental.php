934f26f1fba9d536ebbb26c7642090d254f5dcc2
DennisVN <d.vannieuwenhuyse@gmail.com>
2021-08-03 10:32:38 +0200

    Fix horrible indentation

--- experimental.php
+++ experimental.php
@@ -2,12 +2,11 @@
 
 // fw = for who
 function ordr_pz($pizzatype, $fw) {
-
-$type = $pizzatype;
-echo 'Creating new order... <br>';
-$toPrint = 'A ';
- $toPrint .= $pizzatype;
-$p = calc_cts($type);
+    $type = $pizzatype;
+    echo 'Creating new order... <br>';
+    $toPrint = 'A ';
+    $toPrint .= $pizzatype;
+    $p = calc_cts($type);
 
     $address = 'unknown';
     if($fw == 'koen')
@@ -20,23 +19,19 @@ $p = calc_cts($type);
         $address = 'BeCode office';
     }
 
-        $toPrint .=   ' pizza should be sent to ' . $fw . ". <br>The address: {$address}.";
+    $toPrint .=   ' pizza should be sent to ' . $fw . ". <br>The address: {$address}.";
     echo $toPrint; echo '<br>';
     echo'The bill is €'.$p.'.<br>';
-
-
-
-
     echo "Order finished.<br><br>";
 }
 
-    function total_price($p) {
-            return $p;
-    }
+function total_price($p) {
+    return $p;
+}
 
-        function test($p_type) {
-            echo "Test: type is {$p_type}. <br>";
-        }
+function test($p_type) {
+    echo "Test: type is {$p_type}. <br>";
+}
 
 function calc_cts($p_type)
 {
@@ -44,39 +39,39 @@ function calc_cts($p_type)
 
     if ($p_type == 'marguerita') {
         $cst = 5;
+    }else{
+    if ($p_type == 'golden')
+    {
+        $cst = 100;
     }
-    else
-        {
-        if ($p_type == 'golden')
-        {
-            $cst = 100;
-        }
 
-            if ($p_type == 'calzone')
-            {
-                $cst = 10;
-            }
-
-        if ($p_type == 'hawai') {
-            throw new Exception('Computer says no');
-        }
+    if ($p_type == 'calzone')
+    {
+        $cst = 10;
     }
+
+    if ($p_type == 'hawai') {
+        throw new Exception('Computer says no');
+    }
+        
+    }
+    
 
     return $cst;
 }
 
-            function ordr_piz_all()
-            {
-            $test= 0;
-            ordr_pz('calzone', 'koen');
-            ordr_pz('marguerita', 'manuele');
+function ordr_piz_all()
+{
+$test= 0;
+ordr_pz('calzone', 'koen');
+ordr_pz('marguerita', 'manuele');
 
-            ordr_pz('golden', 'students');
-            }
+ordr_pz('golden', 'students');
+}
 
 function make_Allhappy($do_it) {
 if ($do_it) {
-        ordr_piz_all();
+    ordr_piz_all();
     } else {
         // Should not do anything when false
     }
